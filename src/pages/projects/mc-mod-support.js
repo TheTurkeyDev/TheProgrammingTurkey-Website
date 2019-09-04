6b6d8b3cf6244d4562546a5d00080058ff1df78e
Turkey <turkey2349@gmail.com>
2019-09-04 11:10:37 -0400

    Added key chart to mod-support page

--- src/pages/projects/mc-mod-support.js
+++ src/pages/projects/mc-mod-support.js
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 import PageWrapper from "../base/page-wrapper"
 
 const support = [
-    { bg: "#7CFC00", text: "AS" },
+    { bg: "#7CFC00", text: "S-A" },
     { bg: "#006400", text: "S-NA" },
     { bg: "#FFFF00", text: "IP" },
-    { bg: "#00BFFF", text: "PS" },
+    { bg: "#00BFFF", text: "NS-PS" },
     { bg: "#8B0000", text: "NS-NP" },
+];
 
-]
 const projects = {
     "Chance Cubes": [1, 1, 1, 1, 1, 0, 1, 0],
     "A Block of Charcoal": [1, 4, 4, 1, 1, 0, 1, 0],
@@ -23,7 +23,7 @@ const projects = {
     "Custom UI": [4, 4, 1, 1, 1, 1, 4, 4],
     "TurkeyUtil": [1, 4, 4, 4, 4, 4, 4, 4],
     "Void Compression": [1, 4, 4, 4, 4, 4, 4, 4],
-}
+};
 
 class MCModSupport extends Component {
     constructor(props) {
@@ -34,37 +34,46 @@ class MCModSupport extends Component {
     render() {
         return (
             <PageWrapper>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Mod/ Version</th>
-                            <th scope="col">1.7.10</th>
-                            <th scope="col">1.8</th>
-                            <th scope="col">1.9</th>
-                            <th scope="col">1.10</th>
-                            <th scope="col">1.11</th>
-                            <th scope="col">1.12</th>
-                            <th scope="col">1.13</th>
-                            <th scope="col">1.14</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.keys(projects).map((mod) => {
-                            return (
-                                <tr key={mod}>
-                                    <th scope="row" className="p-1">{mod}</th>
-                                    {projects[mod].map((supportID) => {
-                                        return (
-                                            <td key={`${mod}-${support}`} className="p-1" style={{ backgroundColor: support[supportID].bg }}>
-                                                {support[supportID].text}
-                                            </td>
-                                        )
-                                    })}
-                                </tr>)
-                        })
-                        }
-                    </tbody>
-                </table>
+                <div>
+                    <div className="m-2">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Mod/ Version</th>
+                                    <th scope="col">1.7.10</th>
+                                    <th scope="col">1.8</th>
+                                    <th scope="col">1.9</th>
+                                    <th scope="col">1.10</th>
+                                    <th scope="col">1.11</th>
+                                    <th scope="col">1.12</th>
+                                    <th scope="col">1.13</th>
+                                    <th scope="col">1.14</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.keys(projects).map((mod) => {
+                                    return (
+                                        <tr key={mod}>
+                                            <th scope="row" className="p-1">{mod}</th>
+                                            {projects[mod].map((supportID, index) => {
+                                                return (
+                                                    <td key={`${mod}-${index}`} className="p-1" style={{ backgroundColor: support[supportID].bg }}>
+                                                        {support[supportID].text}
+                                                    </td>
+                                                )
+                                            })}
+                                        </tr>)
+                                })
+                                }
+                            </tbody>
+                        </table>
+                        <div><span className="mt-2" style={{ background: "#7CFC00", padding: "0px 15px 0px 14px" }}>S-A</span><span> = Supported with asctive development</span></div>
+                        <div><span className="mt-2" style={{ background: "#006400", padding: "0px 9px 0px 8px" }}>S-NA</span><span> = Supported, but no active development</span></div>
+                        <div><span className="mt-2" style={{ background: "#FFFF00", padding: "0px 21px 0px 20px" }}>IP</span><span> = Port currently being developed for this version</span></div>
+                        <div><span className="mt-2" style={{ background: "#00BFFF", padding: "0px 5px 0px 5px" }}>NS-PS</span><span> = Not supported, but a port is planned for this version</span></div>
+                        <div><span className="mt-2" style={{ background: "#8B0000", padding: "0px 3px 0px 3px" }}>NS-NP</span><span> = Not supported and this version is not planned to recieve a port</span></div>
+                    </div>
+                </div>
             </PageWrapper>
         );
     }
