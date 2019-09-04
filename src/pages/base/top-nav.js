c34b87ad23c3b70d4b249fd64a620e8064a50f5c
Turkey <turkey2349@gmail.com>
2019-09-04 10:41:45 -0400

    Shuffling around links on nav bar

--- src/pages/base/top-nav.js
+++ src/pages/base/top-nav.js
@@ -18,7 +18,7 @@ class TopNav extends Component {
                         </li>
                         <li className="nav-item dropdown">
                             <Link className="nav-link dropdown-toggle" to="/projects?type=all" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</Link>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown" style={{ marginTop: "-24px" }}>
                                 <Link className="dropdown-item" to="/projects?type=all">All</Link>
                                 <Link className="dropdown-item" to="/projects?type=ld">Ludum Dare</Link>
                                 <Link className="dropdown-item" to="/projects?type=mods">Mods</Link>
@@ -34,8 +34,14 @@ class TopNav extends Component {
                         <li className="nav-item">
                             <a className="nav-link" href="" onClick={() => window.open('http://maven.theprogrammingturkey.com/')}>Maven</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="" onClick={() => window.open('https://discord.gg/wkgyaxs')}>Discord</a>
+                        <li className="nav-item dropdown">
+                            <p className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Other Links</p>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown" style={{ marginTop: "-24px" }}>
+                                <a className="dropdown-item" href="" onClick={() => window.open('https://discord.gg/wkgyaxs')}>Discord</a>
+                                <a className="dropdown-item" href="" onClick={() => window.open('https://twitch.tv/turkey2349')}>Twitch</a>
+                                <a className="dropdown-item" href="" onClick={() => window.open('https://github.com/Turkey2349')}>Github</a>
+                                <a className="dropdown-item" href="" onClick={() => window.open('https://ldjam.com')}>Ludum Dare</a>
+                            </div>
                         </li>
                     </ul>
                 </nav>
