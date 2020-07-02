a523f26974f4efb132c3e1f6a06f66dff06ef4fc
Turkey2349 <turkey2349@gmail.com>
2020-07-02 16:20:15 -0400

    Added backend support to mc mods support page

--- src/pages/projects/mc-mod-support.js
+++ src/pages/projects/mc-mod-support.js
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { PageWrapper } from "../base/page-wrapper"
 
 const support = [
     { bg: "#7CFC00", text: "S-A", desc: "Supported with active development" },
-    { bg: "#006400", text: "S-NA", desc: "Supported, but no active development; Will receive bug fixes if necessary." },
+    { bg: "#006400", text: "S-NA", desc: "Supported, but no active development; Will only receive bug fixes if critical." },
     { bg: "#FFFF00", text: "IP", desc: "Port currently being developed for this version" },
     { bg: "#00BFFF", text: "NS-PS", desc: "Not supported, but a port is planned for this version" },
     { bg: "#8B0000", text: "NS-NP", desc: "Not supported and this version is not planned to recieve a port" },
     { bg: "#E24C00", text: "NS-M", desc: "Not supported and this version might recieve a port in the future, but is unknown" },
 ];
 
-const projects = {
-    "Chance Cubes": [1, 1, 1, 1, 1, 1, 1, 0, 0, 3],
-    "A Block of Charcoal": [1, 4, 4, 1, 1, 1, 1, 0, 0, 3],
-    "A Block of Flint": [4, 4, 4, 4, 4, 1, 1, 0, 0, 3],
-    "Headcrumbs": [1, 4, 1, 1, 1, 1, 4, 5, 5, 5],
-    "Withercrumbs": [1, 4, 4, 1, 4, 1, 4, 4, 5, 5],
-    "Colored Name Tags": [4, 4, 4, 4, 4, 1, 4, 4, 3, 3],
-    "Block Highlighter": [4, 4, 4, 1, 4, 1, 4, 0, 0, 2],
-    "Progression Tweaks": [4, 4, 4, 4, 4, 1, 4, 4, 4, 4],
-    "Gobble Core": [4, 4, 4, 1, 1, 1, 4, 4, 4, 4],
-    "Custom UI": [4, 4, 1, 1, 1, 1, 4, 4, 4, 4],
-    "TurkeyUtil": [1, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-    "Void Compression": [1, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-};
+const versions = [
+    "1.7.10",
+    "1.8",
+    "1.9",
+    "1.10",
+    "1.11",
+    "1.12",
+    "1.13",
+    "1.14",
+    "1.15",
+    "1.16"
+];
+
 
 export function MCModSupport() {
+    const [projects, setProjects] = useState({});
+
+    useEffect(() => {
+        fetch("https://api.theprogrammingturkey.com/projectstatusapi.php")
+            .then(resp => resp.json())
+            .then(json => {
+                let proj = {};
+                json.forEach(status => {
+                    if (!proj[status.mod_name])
+                        proj[status.mod_name] = {};
+                    proj[status.mod_name][status.version] = status.status;
+                });
+                setProjects(proj);
+            })
+    }, []);
+
     return (
         <PageWrapper>
             <div>
@@ -48,14 +63,15 @@ export function MCModSupport() {
                             </tr>
                         </thead>
                         <tbody>
-                            {Object.keys(projects).map((mod) => {
+                            {Object.keys(projects).map(mod => {
                                 return (
                                     <tr key={mod}>
                                         <th scope="row" className="p-1 text-light"> {mod} </th>
-                                        {projects[mod].map((supportID, index) => {
+                                        {versions.map((v, index) => {
+                                            let supportID = projects[mod][v];
                                             return (
                                                 <td key={`${mod}-${index}`} className="p-1 text-center" style={{ backgroundColor: support[supportID].bg }}>
-                                                    {support[supportID].text}
+                                                    {support[parseInt(supportID)].text}
                                                 </td>
                                             )
                                         })}
