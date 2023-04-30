935a9200106dc8c2b5d27515cd778bff95d1171b
TheTurkeyDev <turkey@theturkey.dev>
2023-04-30 13:05:46 -0400

    Updated f1 standings

--- src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
+++ src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
@@ -68,15 +68,15 @@ const teams: readonly F1Constructor[] = [
 ];
 
 const liveStandings: { readonly [key: string]: { readonly p: number, readonly tb: number } } = {
-    rb: { p: 123, tb: 0 },
-    am: { p: 65, tb: 0 },
-    merc: { p: 56, tb: 0 },
-    ferrari: { p: 26, tb: 0 },
-    mclaren: { p: 12, tb: 0 },
+    rb: { p: 180, tb: 0 },
+    am: { p: 87, tb: 0 },
+    merc: { p: 76, tb: 0 },
+    ferrari: { p: 62, tb: 0 },
+    mclaren: { p: 14, tb: 0 },
     alpine: { p: 8, tb: 0 },
     haas: { p: 7, tb: 0 },
     ar: { p: 6, tb: 0 },
-    at: { p: 1, tb: 1 },
+    at: { p: 2, tb: 0 },
     williams: { p: 1, tb: 0 },
 };
 
