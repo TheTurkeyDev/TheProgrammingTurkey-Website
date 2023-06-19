32ee4c420f373037c738f81d8c7c064f6ce2fba0
TheTurkeyDev <turkey@theturkey.dev>
2023-06-19 19:15:36 -0400

    Updated F1 standings. I really should move this to a DB...

--- src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
+++ src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
@@ -68,16 +68,16 @@ const teams: readonly F1Constructor[] = [
 ];
 
 const liveStandings: { readonly [key: string]: { readonly p: number, readonly tb: number } } = {
-    rb: { p: 289, tb: 0 },
-    merc: { p: 152, tb: 0 },
-    am: { p: 134, tb: 0 },
-    ferrari: { p: 100, tb: 0 },
-    alpine: { p: 40, tb: 0 },
+    rb: { p: 321, tb: 0 },
+    merc: { p: 167, tb: 0 },
+    am: { p: 154, tb: 0 },
+    ferrari: { p: 122, tb: 0 },
+    alpine: { p: 44, tb: 0 },
     mclaren: { p: 17, tb: 0 },
-    haas: { p: 8, tb: 1 },
-    ar: { p: 8, tb: 0 },
+    ar: { p: 9, tb: 0 },
+    haas: { p: 8, tb: 0 },
+    williams: { p: 7, tb: 0 },
     at: { p: 2, tb: 0 },
-    williams: { p: 1, tb: 0 },
 };
 
 const picks: { readonly [key: string]: readonly string[] } = {
