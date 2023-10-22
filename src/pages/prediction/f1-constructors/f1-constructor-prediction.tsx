2bbd6779e86597357598819e568fc07ad592bf90
TheTurkeyDev <turkey2349@gmail.com>
2023-10-22 16:53:34 -0400

    Updated f1 standings

--- src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
+++ src/pages/prediction/f1-constructors/f1-constructor-prediction.tsx
@@ -68,16 +68,16 @@ const teams: readonly F1Constructor[] = [
 ];
 
 const liveStandings: { readonly [key: string]: { readonly p: number, readonly tb: number } } = {
-    rb: { p: 623, tb: 0 },
-    merc: { p: 305, tb: 0 },
-    ferrari: { p: 285, tb: 0 },
-    am: { p: 221, tb: 0 },
-    mclaren: { p: 172, tb: 0 },
-    alpine: { p: 84, tb: 0 },
-    williams: { p: 21, tb: 0 },
+    rb: { p: 704, tb: 0 },
+    merc: { p: 358, tb: 0 },
+    ferrari: { p: 327, tb: 0 },
+    mclaren: { p: 239, tb: 0 },
+    am: { p: 232, tb: 0 },
+    alpine: { p: 96, tb: 0 },
+    williams: { p: 23, tb: 0 },
+    ar: { p: 16, tb: 0 },
     haas: { p: 12, tb: 0 },
-    ar: { p: 10, tb: 0 },
-    at: { p: 5, tb: 0 },
+    at: { p: 7, tb: 0 },
 };
 
 const picks: { readonly [key: string]: readonly string[] } = {
